36cd7bc7622f53d31ddfd33bceff69aaa1f336d8
sidhantkaushal <kaushalsidhant434@gmail.com>
2022-11-04 01:04:33 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// The above statement create a root for a new virtual dom tree which will be used and it assigned to root.
 root.render(
   <React.StrictMode>
     <App />
