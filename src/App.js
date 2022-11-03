57d8d6906fe75a715d82a987cdc559f7e9ed4b05
sidhantkaushal <kaushalsidhant434@gmail.com>
2022-11-04 05:12:38 +0530

    class8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,98 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Card from "./components/Card";
+
+const data = [
+  {
+    url:
+      "https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-04/Man%20United%20squad%20list.jpg?itok=zlwqh7Cb",
+    prodName: "kicks",
+    _id: 1,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+  {
+    url:
+      "https://www.telegraph.co.uk/content/dam/football/2019/05/01/TELEMMGLPICT000195477369_trans_NvBQzQNjv4BqeK8ehqBZJSTiVTgumtathbH8AD1LYTdJsoz8pklmEgw.jpeg",
+    prodName: "jerseys",
+    _id: 2,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+  {
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    prodName: "jackets",
+    _id: 3,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+  {
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    prodName: "hoodies",
+    _id: 4,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+  {
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    prodName: "hoodies",
+    _id: 4,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+  {
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    prodName: "hoodies",
+    _id: 4,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+  {
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    prodName: "hoodies",
+    _id: 4,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+  {
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    prodName: "hoodies",
+    _id: 4,
+    class: 32,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae commodi, autem iusto neque reiciendis dolores harum, rerum quae, quia laudantium. Sed dignissimos minus eaque perferendis, magni neque similique. Quibusdam?",
+  },
+];
 
 function App() {
-  return <h1>Hello World</h1>;
+  const title = "we rockk";
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <br />
+      <div className="row">
+        {data.map((val, i) => {
+          return (
+            <div className="col-6 col-sm-6 col-md-4 p-3">
+              <Card values={val} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,46 @@
-import React from "react";
+// import React from "react";
 
-export default function Card() {
-  return <div>Card</div>;
+// // https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png
+
+// // eslint-disable-next-line no-unused-vars
+// const Card = (props) => {
+//   console.log(props);
+//   //Props destructing
+//   const { _id, desc, prodName, url } = props.values;
+//   return (
+//     <div class="card">
+//       <img class="card-img-top" src={url} alt="Card cap" />
+//       <div class="card-body">
+//         <h5 class="card-title">{props.values.prodName}</h5>
+//         <p class="card-text">{props.values.desc}</p>
+//         <button class="btn btn-primary"> {_id}</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+import React, { Component } from "react";
+
+export class Card extends Component {
+  render() {
+    const { _id, desc, url, prodName } = this.props.values;
+
+    this.state = {
+      btnText: "Buy" + _id,
+    };
+    return (
+      <div class="card">
+        <img class="card-img-top" src={url} alt="Card cap" />
+        <div class="card-body">
+          <h5 class="card-title">{prodName}</h5>
+          <p class="card-text">{desc}</p>
+          <button class="btn btn-primary"> {this.state.btnText}</button>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Card;
